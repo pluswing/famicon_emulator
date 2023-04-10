296f26cf06c4646bd859d3c777beb5751ecaa71a
pluswing <toshiyuki@pluswing.co.jp>
2023-04-10 09:13:49 +0900

    #057 AddrRegister写経

--- src/ppu.rs
+++ src/ppu.rs
@@ -1,8 +1,71 @@
 pub struct NesPPU {
     pub chr_rom: Vec<u8>,
-    pub palette_talbe: [u8; 32],
+    pub palette_table: [u8; 32],
     pub vram: [u8; 2048],
     pub oam_data: [u8; 256],
 
     pub mirroring: Mirroring,
+}
+
+impl NesPPU {
+    pub fn new(chr_rom: Vec<u8>, mirroring: Mirroring) -> self {
+        NesPPU {
+            chr_rom: chr_rom,
+            mirroring: mirroring,
+            vram: [0; 2048],
+            oam_data: [0; 64 * 4],
+            palette_table: [0; 32],
+        }
+    }
+}
+
+pub struct AddrRegister {
+    value: (u8, u8),
+    hi_ptr: bool,
+}
+
+impl AddrRegister {
+    pub fn new() -> Self {
+        AddrRegister {
+            value: (0, 0),
+            hi_ptr: true,
+        }
+    }
+
+    fn set(&mut self, data: u16) {
+        self.value.0 = (data >> 8) as u8;
+        self.value.1 = (data & 0xFF) as u8;
+    }
+
+    pub fn update(&mut self, data: u8) {
+        if self.hi_ptr {
+            self.value.0 = data;
+        } else {
+            self.value.1 = data;
+        }
+
+        if self.get() > 0x3FFF {
+            self.set(self.get() & 0b11_1111_1111_1111);
+        }
+        self.hi_ptr = !self.hi_ptr;
+    }
+
+    pub fn increment(&mut self, inc: u8) {
+        let lo = self.value.1;
+        self.value.1 = self.value.1.wrapping_add(inc);
+        if lo > self.value.1 {
+            self.value.0 = self.value.0.wrapping_add(1);
+        }
+        if self.get() > 0x3FFF {
+            self.set(self.get() & 0b11_1111_1111_1111);
+        }
+    }
+
+    pub fn reset_latch(&mut self) {
+        self.hi_ptr = true;
+    }
+
+    pub fn get(&self) -> u16 {
+        ((self.value.0 as u16) << 8) | (self.value.1 as u16)
+    }
 }
